96003e8a7ddf36ed3cb80ab2f61ddda1fa027b93
Antoniya Nemtserova <anemtserova@gmail.com>
2021-10-10 15:51:46 -0400

    added real text to About page

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,13 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 
 export const About = () => {
-  return (
-    <Fragment>
-      <h1>About this app</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod eos veritatis, ducimus est, odit sint delectus vitae exercitationem laboriosam ullam inventore maxime accusantium itaque!</p>
-      <p>Version: 1.0.0</p>
-    </Fragment>
-  )
-}
+	return (
+		<Fragment>
+			<h1>About this app</h1>
+			<p>
+				<b>Github Profile Finder</b>is an easy-to-use app that allows you to
+				search for github users by user or real name and review their profile
+				details. The app also provides links to up to five repos of each user.
+			</p>
+			<p>This is version: 1.0.0</p>
+		</Fragment>
+	);
+};
 
 export default About;
